9e4a854d4cdb38208a9e7bb84688b635d9399dc1
KemakornS <kemakorn.s@gmail.com>
2016-06-01 00:23:40 +0700

    open web driver and count word

--- src/main/java/com/kem/WordCountWebDriver/WordCountWeb.java
+++ src/main/java/com/kem/WordCountWebDriver/WordCountWeb.java
@@ -1,11 +1,17 @@
 package com.kem.WordCountWebDriver;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.util.HashMap;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by KEM-PC on 30/5/2559.
@@ -15,6 +21,13 @@ public class WordCountWeb {
     private String url = "http://wordcounttools.com/";
     private WebDriver driver;
     private WordCountWeb(){
+        /*String chromepath = String.join("\\",new String[]{System.getProperty("user.dir"),"target","chromedriver.exe"});
+        System.setProperty("webdriver.chrome.driver", chromepath);
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        ChromeOptions options = new ChromeOptions();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
+        driver = new ChromeDriver(capabilities);*/
         driver = new FirefoxDriver();
         driver.get(url);
     }
@@ -26,9 +39,65 @@ public class WordCountWeb {
         return instance;
     }
 
-    public HashMap<String, Integer> getCountResult(String text){
+    public ArrayList<WordDensity> getCountResult(String text){
         WebElement textbox = driver.findElement(By.id("textbox"));
-        textbox.sendKeys(text);
-        return null;
+        StringSelection stringSelection = new StringSelection(text);
+        Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clpbrd.setContents(stringSelection, null);
+        textbox.sendKeys(Keys.chord(Keys.CONTROL, "v"));
+        return readResult();
     }
+
+    public void setTopWord(int number){
+        WebElement textbox = driver.findElement(By.id("numTopKeyWord"));
+        textbox.sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        textbox.sendKeys(Keys.DELETE);
+        textbox.sendKeys(Integer.toString(number));
+    }
+
+    private ArrayList<WordDensity> readResult(){
+        java.util.List<WebElement> wordElems = driver.findElements(By.xpath("//div[@id='densityList']//label"));
+        String patternStr = "<strong>(.*?)<\\/strong> - (\\d+) .*?(\\d+\\.\\d+)%";
+        Pattern pattern = Pattern.compile(patternStr);
+        ArrayList<WordDensity> wordDensityMap = new ArrayList<>();
+        for (WebElement ele:wordElems ) {
+            String html = ele.getAttribute("outerHTML");
+            Matcher matcher = pattern.matcher(html);
+            if(matcher.find()){
+                WordDensity d = new WordDensity(matcher.group(1),
+                        Integer.parseInt(matcher.group(2)), Double.parseDouble(matcher.group((3))));
+                wordDensityMap.add(d);
+            }
+        }
+        return wordDensityMap;
+    }
+
+    public class WordDensity{
+        String word;
+        int density = 0;
+        double densityPct = 0.0;
+        public WordDensity(String word, int density, double densityPct){
+            this.word = word;
+            this.density = density;
+            this.densityPct = densityPct;
+        }
+
+        public String getWord() {
+            return word;
+        }
+
+        public int getDensity() {
+            return density;
+        }
+
+        public double getDensityPct() {
+            return densityPct;
+        }
+
+        public String toString(){
+            return String.format("%s - %s times (%s%\u0025)", word, density, densityPct);
+        }
+    }
+
+
 }
--- src/test/java/BugReadTest/SimpleTest.java
+++ src/test/java/BugReadTest/SimpleTest.java
@@ -18,7 +18,7 @@ public class SimpleTest {
     private BugRead reader;
     @BeforeClass
     public void setUp() {
-        String dirPath = "C:\\Users\\KATSU\\docker\\data\\apache\\blocker";
+        String dirPath = "C:\\Users\\KEM-PC\\docker\\data\\apache\\blocker";
         reader = new BugRead(dirPath);
     }
 
@@ -30,8 +30,9 @@ public class SimpleTest {
     @Test(groups = { "fast" })
     public void PlainTextTest() throws IOException {
         ArrayList<String> result = reader.ReadText(50);
-        String allText = BugRead.writeToFile(result, "C:\\Users\\KATSU\\docker\\data\\apache\\0.blocker50.txt");
-        HashMap<String, Integer> countResult = WordCountWeb.getInstance().getCountResult(allText);
+        String allText = BugRead.writeToFile(result, "C:\\Users\\KEM-PC\\docker\\data\\apache\\0.blocker50.txt");
+        WordCountWeb.getInstance().setTopWord(50);
+        ArrayList<WordCountWeb.WordDensity> countResult = WordCountWeb.getInstance().getCountResult(allText);
     }
 
 }
