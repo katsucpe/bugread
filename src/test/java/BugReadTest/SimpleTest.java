634fbedebd374ee5325dd13060369d1c0a578912
KemakornS <katsu.cpe@gmail.com>
2016-07-17 19:49:32 +0700

    add step for read bug

--- src/test/java/BugReadTest/SimpleTest.java
+++ src/test/java/BugReadTest/SimpleTest.java
@@ -70,10 +70,16 @@ public class SimpleTest {
     @Test(groups = {"fast"})
     public void ProcessTextForClassificationTest() throws IOException {
         String path = "C:\\Users\\KATSU\\docker\\data\\Eclipse";
-        reader = new BugReadForClass(String.join("\\", new String[]{path, "blocker"}));
+        String[] severityList = new String[] {"blocker", "critical", "major", "normal", "minor", "trivial" };
+        for (String serv: severityList ) {
+            processBugClassification(path, serv, 100);
+        }
+    }
 
-        ArrayList<String> result = reader.ReadText(100);
-        String allText = reader.writeToFile(result, String.format("%s\\0.%s.%s.ForClass.csv", path, 100, "blocker"));
+    private void processBugClassification(String path, String severity, int limit)  throws IOException {
+        reader = new BugReadForClass(String.join("\\", new String[]{path, severity}));
+        ArrayList<String> result = reader.ReadText(limit);
+        String allText = reader.writeToFile(result, String.format("%s\\1.%s.%s.ForClass.csv", path, limit, severity));
     }
 }
 
